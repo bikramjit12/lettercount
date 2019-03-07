7badd84eac2675ed1b8741119ea66fb256bfa1b2
Bikramjit Singh Mall <BM042@atcltd.atc.org.nz>
2019-03-07 13:54:30 +1300

    Add project files.

---
+++ lettercount/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace lettercount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxLetterCount = new System.Windows.Forms.ListBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxLetterCount
+            // 
+            this.lbxLetterCount.FormattingEnabled = true;
+            this.lbxLetterCount.Location = new System.Drawing.Point(616, 34);
+            this.lbxLetterCount.Name = "lbxLetterCount";
+            this.lbxLetterCount.Size = new System.Drawing.Size(155, 277);
+            this.lbxLetterCount.TabIndex = 0;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(223, 146);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(100, 23);
+            this.btnCount.TabIndex = 1;
+            this.btnCount.Text = "Count Letter";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 146);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(419, 146);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear List";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.lbxLetterCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxLetterCount;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ lettercount/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lettercount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Dictionary<string, string> DictionaryWords = new Dictionary<string,
+string>();
+            InitializeComponent();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            var Letters = new Dictionary<char, int>();
+            string word = File.ReadAllText("ty.txt");
+                //txtBox.Text;
+ // for each letter in the word
+ foreach (char a in word)
+            {
+//if there is no entry in the dictionary then add a new entry with 1 as the counter
+ if (Letters.ContainsKey(a) == false)
+                {
+                    Letters.Add(a, 1);
+                }
+                else
+                // if its already in the dictionary increase the number (value) by one.
+                {
+                    //finds the key and adds 1 to the value, basically counts up
+                    Letters[a]++;
+                }
+            }
+            //loop through the dictionary and get out the key/value pairs
+
+            foreach (KeyValuePair<char, int> pair in Letters)
+            {
+                lbxLetterCount.Items.Add(pair.Key + " --> " + pair.Value);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbxLetterCount.Items.Clear();
+        }
+    }
+}
